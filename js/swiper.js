ebe3c36a83ad3dfc83ed5ae3b3a6df0206db284d
chenxi-debugger <chenxi.debugger@gmail.com>
2025-07-04 20:53:20 -0700

    swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,60 +1,52 @@
-// 大图swiper部分
-var swiper = new Swiper(".mySwipers", {
-    pagination: {
-        el: ".swiper-pagination1",
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + "</span>";
-        },
-    },
-    // 左右按钮
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
 
-
-const swiperImgs = document.querySelector('.swiperImgs')
-const pics = document.querySelectorAll('.pic')
-const closeSwiper = document.getElementById('closeSwiper')
-
-// 鼠标移上去会图片移动
-swiperImgs.addEventListener('mouseover', function (e) {
-    // 获取鼠标x轴位置
-    // console.log(e.clientX);
-    // console.log(swiperImg.offsetWidth / 2);
-    if (e.clientX > swiperImgs.offsetWidth / 2) {
-        swiperImgs.style.marginLeft = '-960px'
-    } else {
-        swiperImgs.style.marginLeft = '0px'
-    }
-})
-swiperImgs.addEventListener('mouseout', function (e) {
-    swiperImgs.style.marginLeft = '-480px'
-})
-// 悬浮照片上 会出现文字和图标
-pics.forEach(pic => {
-    pic.addEventListener('mouseover', function () {
-        const imgIcon = this.querySelector('.imgIcon')
-        imgIcon.style.display = 'block'
-    })
-    pic.addEventListener('mouseout', function () {
-        const imgIcon = this.querySelector('.imgIcon')
-        imgIcon.style.display = 'none'
-    })
-    // 点击图片出现大图
-    pic.addEventListener('click', function (e) {
-        // console.log(e.target);
-        // console.log(e.target.dataset);
-        swiper.slideTo(e.target.dataset.index)
-        bigSwiper.style.display = 'block';
-    })
-})
-
-// 关闭大图
-closeSwiper.addEventListener('click', function () {
-    // console.log('@@@');
-    bigSwiper.style.display = 'none';
-
-})
+        window.addEventListener('load', function () {
+          var swiper = new Swiper(".mySwipers", {
+            pagination: {
+              el: ".swiper-pagination1",
+              clickable: true,
+              renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+              },
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            },
+          });
+      
+          const swiperImgs = document.querySelector('.swiperImgs');
+          const pics = document.querySelectorAll('.pic');
+          const closeSwiper = document.getElementById('closeSwiper');
+          const bigSwiper = document.getElementById('bigSwiper');
+      
+          swiperImgs.addEventListener('mouseover', function (e) {
+            if (e.clientX > swiperImgs.offsetWidth / 2) {
+              swiperImgs.style.marginLeft = '-960px';
+            } else {
+              swiperImgs.style.marginLeft = '0px';
+            }
+          });
+      
+          swiperImgs.addEventListener('mouseout', function () {
+            swiperImgs.style.marginLeft = '-480px';
+          });
+      
+          pics.forEach(pic => {
+            pic.addEventListener('mouseover', function () {
+              const imgIcon = this.querySelector('.imgIcon');
+              imgIcon.style.display = 'block';
+            });
+            pic.addEventListener('mouseout', function () {
+              const imgIcon = this.querySelector('.imgIcon');
+              imgIcon.style.display = 'none';
+            });
+            pic.addEventListener('click', function (e) {
+              swiper.slideTo(Number(e.target.dataset.index));
+              bigSwiper.style.display = 'block';
+            });
+          });
+      
+          closeSwiper.addEventListener('click', function () {
+            bigSwiper.style.display = 'none';
+          });
+        });
